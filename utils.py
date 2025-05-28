dc2ea5b33113e2a9c6514cfa98c74821d6b02f06
e.kireev <ekireev75@gmail.com>
2025-05-29 00:19:46 +0300

    made k-modes

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import numpy as np
+from scipy.spatial.distance import hamming
+from itertools import combinations
 
 
 def multiple_graphs(n_cols: int, target: str, data: pd.DataFrame):
@@ -10,3 +13,16 @@ def multiple_graphs(n_cols: int, target: str, data: pd.DataFrame):
         plt.subplot(rows, n_cols, i)
         sns.lineplot(x=target, y=f, data=data)
     plt.show()
+
+
+# Расстояние между кластерами (Inter-Cluster Distance)
+# Показывает, насколько кластеры разделены. Чем больше расстояние, тем лучше.
+def inter_cluster_distance(centroids):
+    distances = []
+    for (c1, c2) in combinations(centroids, 2):
+        distances.append(hamming(c1, c2))
+    return np.mean(distances)
+
+
+
+
